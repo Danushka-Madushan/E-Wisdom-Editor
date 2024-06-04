a726f3c41ca50470bf8e76e52e483e192990e7e8
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-04 18:33:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,79 @@
-import { Accordion, AccordionItem, Button } from "@nextui-org/react";
-import { FolderDown } from 'lucide-react';
+import { Tabs, Tab, Input, Link, Button } from "@nextui-org/react";
+import { Contact, PencilRuler } from 'lucide-react';
+import { useState } from 'react';
 
-const App = () => {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+export default function App() {
+  const [selected, setSelected] = useState<React.Key>("student");
 
   return (
-    <div className='m-11'>
-      <Accordion variant="shadow">
-        <AccordionItem key="1" aria-label="Accordion 1" title="Grade 11 English Papers">
-          <Button color="success" className='text-white text-base' endContent={<FolderDown />}>
-            Download
-          </Button>
-        </AccordionItem>
-        <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-          {defaultContent}
-        </AccordionItem>
-      </Accordion>
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Tabs aria-label="Options" color="primary"
+          selectedKey={selected.toString()}
+          onSelectionChange={setSelected}
+        >
+          <Tab
+            key="student"
+            title={
+              <div className="flex items-center space-x-2">
+                <Contact />
+                <span>Student</span>
+              </div>
+            }
+          >
+            <form className="flex flex-col gap-4">
+              <Input isRequired label="Email" placeholder="Enter your email" type="email" />
+              <Input
+                isRequired
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+              />
+              <p className="text-center text-small">
+                Need to create an account?{" "}
+                <Link size="sm" onPress={() => setSelected("sign-up")}>
+                  Sign up
+                </Link>
+              </p>
+              <div className="flex gap-2 justify-end">
+                <Button fullWidth color="primary">
+                  Login
+                </Button>
+              </div>
+            </form>
+          </Tab>
+          <Tab
+            key="editor"
+            title={
+              <div className="flex items-center space-x-2">
+                <PencilRuler />
+                <span>Editor</span>
+              </div>
+            }
+          >
+            <form className="flex flex-col gap-4">
+              <Input isRequired label="Email" placeholder="Enter your email" type="email" />
+              <Input
+                isRequired
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+              />
+              <p className="text-center text-small">
+                Need to create an account?{" "}
+                <Link size="sm" onPress={() => setSelected("sign-up")}>
+                  Sign up
+                </Link>
+              </p>
+              <div className="flex gap-2 justify-end">
+                <Button fullWidth color="primary">
+                  Login
+                </Button>
+              </div>
+            </form>
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
-
-export default App
