31b4f827f103ad6043634a9f769ebf59476853c5
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-05 11:33:05 +0530

    feat: create LoginModel.tsx

---
+++ src/components/Login/LoginModel.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Card, CardBody, Tab, Tabs } from '@nextui-org/react'
+import { ContactRound, PenTool } from 'lucide-react'
+import LoginForm from './LoginForm'
+import { useState } from 'react'
+
+const LoginModel = () => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const onFormChange = (target: HTMLInputElement) => {
+    const { type, value } = target
+    setFormState({
+      ...formState,
+      [type]: value
+    })
+  } 
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <Card className="max-w-full w-[340px]">
+        <CardBody className="overflow-hidden">
+          <div className='w-full flex flex-col items-center gap-y-2 justify-center my-4'>
+            <Avatar isBordered color="primary" radius="full" showFallback name='Σ' className="w-20 h-20 text-large" src="https://i.ibb.co/nRyJXF7/sigma.png" />
+            <span className='text-xl text-gray-700'>E Wisdom Institiute</span>
+          </div>
+          <Tabs
+            color="primary"
+            variant="bordered"
+            fullWidth
+            size="md"
+            aria-label="Tabs form"
+          >
+            <Tab key="login" title={
+              <div className="flex items-center space-x-2">
+                <ContactRound />
+                <span>Student</span>
+              </div>}
+            >
+              <LoginForm email={formState.email} password={formState.password} onFormChange={onFormChange} />
+            </Tab>
+            <Tab key="sign-up" title={
+              <div className="flex items-center space-x-2">
+                <PenTool />
+                <span>Editor</span>
+              </div>}
+            >
+              <LoginForm email={formState.email} password={formState.password} onFormChange={onFormChange} />
+            </Tab>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+export default LoginModel
