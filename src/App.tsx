a7db8085eb13b4a8f8d333248e2ce79b72324dc7
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-09 20:01:29 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import { useAuth } from './hooks/useAuth';
 import Sidebar from './components/Sidebar/Sidebar'
 import Students from './pages/Students';
 import Subscriptions from './pages/Subscriptions';
 import Courses from './pages/Courses';
 import Videos from './pages/Videos';
 import Dashboard from './pages/Dashboard';
+import AuthProvider from './Auth/AuthContext';
+import ProtectedRoute from './Auth/ProtectedRoute';
+import LoginModel from './components/Login/LoginModel';
 
 const Routers = [
   {
@@ -34,18 +38,29 @@ const Routers = [
 ]
 
 const App = () => {
+  const { isAuthenticated } = useAuth()
+
   return (
     <div className='flex'>
-      <BrowserRouter>
-        <div className='h-screen'>
+      <BrowserRouter>.
+        {isAuthenticated && <div className='h-screen'>
           <Sidebar />
-        </div>
+        </div>}
         <Routes>
-          {Routers.map(({ path, element }, i) => <Route key={i} path={path} element={element} />)}
+          <Route path="/login" element={<LoginModel />} />
+          {Routers.map(({ path, element }, i) => <Route key={i} path={path} element={<ProtectedRoute element={element}/>} />)}
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
 
-export default App
+const ProtectedApp = () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  )
+}
+
+export default ProtectedApp
