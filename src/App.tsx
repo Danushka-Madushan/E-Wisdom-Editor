2343f1335eee9d2e2ed728ef4b94b41bdcf568ef
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-09 18:50:10 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import { Fragment } from 'react/jsx-runtime'
-import LoginModel from './components/Login/LoginModel'
-import Sidebar from './components/Sidebar'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Sidebar from './components/Sidebar/Sidebar'
+import Students from './pages/Students';
+import Subscriptions from './pages/Subscriptions';
+import Courses from './pages/Courses';
+import Videos from './pages/Videos';
+import Dashboard from './pages/Dashboard';
+
+const Routers = [
+  {
+    path: '/',
+    element: <Dashboard />
+  },
+  {
+    path: '/students',
+    element: <Students />
+  },
+  {
+    path: '/subscriptions',
+    element: <Subscriptions />
+  },
+  {
+    path: '/courses',
+    element: <Courses />
+  },
+  {
+    path: '/videos',
+    element: <Videos />
+  }
+]
 
 const App = () => {
   return (
-    <Fragment>
-      <Sidebar />
-      <LoginModel />
-    </Fragment>
+    <div className='flex'>
+      <BrowserRouter>
+        <div className='h-screen'>
+          <Sidebar />
+        </div>
+        <Routes>
+          {Routers.map(({ path, element }, i) => <Route key={i} path={path} element={element} />)}
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
