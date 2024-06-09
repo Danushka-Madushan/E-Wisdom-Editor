f32fe3128655ab02e692c2e968de1bb849ce12a4
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-09 18:50:35 +0530

    feat: create LogoutIcon.tsx

---
+++ src/components/Icons/LogoutIcon.tsx
@@ -0,0 +1,7 @@
+const LogoutIcon = ({ size }: { size: number }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} className='-rotate-90' viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"><path d="M4 12a8 8 0 1 0 16 0" opacity=".5" /><path strokeLinejoin="round" d="M12 14V4m0 0l3 3m-3-3L9 7" /></g></svg>
+  )
+}
+
+export default LogoutIcon
