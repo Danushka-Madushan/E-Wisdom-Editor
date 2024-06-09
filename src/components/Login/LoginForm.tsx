11f9d7ee6b8c724c0417cefb87c8d3c9ca33c1d5
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-06-09 22:57:47 +0530

    update LoginForm.tsx

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,8 +1,9 @@
-import { Button, Input, Link, Spinner } from '@nextui-org/react'
+import { Button, Input, Link, Spinner, useDisclosure } from '@nextui-org/react'
 import { Dispatch, Key, SetStateAction, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useAuth } from '../../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
+import SigninModel from '../Signin/SigninModel'
 
 const TabKeys = ['student', 'editor'] as const
 
@@ -16,6 +17,7 @@ interface LoginFormProps {
 
 const LoginForm = ({ email, password, onFormChange, ActiveTab, SetNoNActiveTab }: LoginFormProps) => {
   const [isLoading, setLoading] = useState(false)
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const { login } = useAuth()
   const Navigate = useNavigate()
 
@@ -25,12 +27,13 @@ const LoginForm = ({ email, password, onFormChange, ActiveTab, SetNoNActiveTab }
       <Input onChange={({ target }) => onFormChange(target)} value={password} isRequired label="Password" size='lg' placeholder="Enter your password" type="password" />
       {ActiveTab === 'student' ? <p className="text-center text-small">
         Need to create an account?{" "}
-        <Link size="sm" className='cursor-pointer'>
+        <Link size="sm" className='cursor-pointer' onPress={onOpen}>
           Sign up
+          <SigninModel isOpen={isOpen} onOpenChange={onOpenChange}/>
         </Link>
       </p> : false}
       <div className="flex gap-2 justify-end">
-        <Button onClick={() => setLoading((prev) => {
+        <Button onPress={() => setLoading((prev) => {
           if (!prev) {
             SetNoNActiveTab(TabKeys.filter(item => item !== ActiveTab))
             if (login) {
